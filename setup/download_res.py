e8609bc077ddfc28a0883b1b5392e27b470dc293
danilotat <danilotatoni@gmail.com>
2024-11-21 16:18:32 +0100

    feat: genome dict after downloading

--- setup/download_res.py
+++ setup/download_res.py
@@ -248,7 +248,19 @@ def update_yaml(conf_main: str, resources: str, outfolder: str):
                 # genome, transcriptome and gtf doesn't need conversions
                 if resource_entry.res_type in ["fasta", "gtf"]:
                     resource_entry._download_stuff()
-                elif resource_entry.main_filename == "dbsnps":
+                    # if it's a genome, do also the dictionary
+                    if "genome" in resource_entry.main_filename:
+                        outfile = os.path.join(outfolder, resource_entry.main_filename.replace('.fa.gz', '.dict'))
+                        subprocess.run(
+                            [
+                                'gatk',
+                                'CreateSequenceDictionary',
+                                '-R',
+                                os.path.join(outfolder, resource_entry.main_filename),
+                                '-O',
+                                outfile
+                            ])
+                elif resource_entry.res_name == "dbsnps":
                     # for dbsnps we need to do conversion and then annotation for allele frequency
                     refseq_conv_table = os.path.join(
                         os.path.dirname(cwd), "refseq_dbsnp.tsv"
@@ -285,8 +297,8 @@ def update_yaml(conf_main: str, resources: str, outfolder: str):
                             os.path.abspath(outfolder)
                         ]
                     )
-                    resource_entry.main_filename = resource_entry.main_filename.replace(
-                        ".tar.gz", ""
+                    resource_entry.main_filename = os.path.join(
+                        outfolder, 'homo_sapiens'
                     )
                 # update entry accordingly
                 conf_main_yaml["resources"][res_name] = os.path.join(
